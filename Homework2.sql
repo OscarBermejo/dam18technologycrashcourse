31d687b1bde5fdbd4b29fe8cf90a996293d6aae8
OscarBermejo <obermejo@live.com>
2018-11-30 12:31:19 +0100

    sql_masterclass

---
+++ Homework2.sql
@@ -0,0 +1,4 @@
+SELECT (acos(sin(radians(41.385)) * sin(radians(40.4168)) + 
+cos(radians(41.385)) * cos(radians(40.4168)) * 
+cos(radians(-3.7038) - radians(2.1734))) * 6378) as 
+Barcelona_Madrid;
